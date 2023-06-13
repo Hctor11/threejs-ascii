38e4837ebedc4282bc6c27addfbeb67df5b500ca
hector <hectorrrivera1010@gmail.com>
2023-06-12 21:17:34 -0600

    added sphere in canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import ThreeScene from './Components/ThreeScene'
+import Sphere from './Components/Sphere'
 
 function App() {
 
   return (
     <>
       <ThreeScene>
+        <Sphere/>
+        <ambientLight/>
       </ThreeScene>
     </>
   )
---
+++ src/Components/Sphere.jsx
@@ -0,0 +1,12 @@
+
+
+const Sphere = () => {
+  return (
+    <mesh>
+        <sphereGeometry/>
+        <meshStandardMaterial wireframe color="#fcf"/>
+    </mesh>
+  )
+}
+
+export default Sphere
